42d15be28eb44d1e0daa4d1e29a1c7a434b5a005
pop <gggoodgame219@gmail.com>
2024-12-02 22:43:21 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import errorHandling from "./middlewares/errorHandler.js";
+import createUserTable from "./data/createUserTable.js";
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api", userRoutes);
+
+// Error handling middleware
+app.use(errorHandling);
+
+// Create table before starting server
+createUserTable();
+
+
+// Testing Postgres connection
+app.get("/", async(req, res) => {
+    const result = await pool.query("SELECT current_database()");
+    res.send(`The database name is : ${result.rows[0].current_database}`)
+})
+
+// Server running
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`)
+})
